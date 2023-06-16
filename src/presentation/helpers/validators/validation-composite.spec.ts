76dd0ca88c1c0b8189e6427930294eae49eb23d2
Willian Trevisol <willianbylla@hotmail.com>
2023-06-16 17:39:37 -0300

    test: add tests to ValidationComposite

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,55 @@
+import { type Validation } from '../../controllers/signup/signup-protocols'
+import { MissingParameterError } from '../../errors'
+import { ValidationComposite } from './validation-composite'
+
+interface SutTypes {
+  systemUnderTest: ValidationComposite
+  validationStubs: Validation[]
+}
+
+const validationStubFactory = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (data: any): any {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const sutFactory = (): SutTypes => {
+  const validationStubs = [validationStubFactory(), validationStubFactory()]
+  const systemUnderTest = new ValidationComposite(validationStubs)
+  return {
+    systemUnderTest,
+    validationStubs
+  }
+}
+
+describe('ValidationComposite', () => {
+  test('Should return an error if validation fails', () => {
+    const { systemUnderTest, validationStubs } = sutFactory()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(
+      new MissingParameterError('field')
+    )
+    const error = systemUnderTest.validate({ field: 'any_value' })
+    expect(error).toEqual(new MissingParameterError('field'))
+  })
+
+  test('Should return the first error if more than one validation fails', () => {
+    const { systemUnderTest, validationStubs } = sutFactory()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(
+      new Error()
+    )
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(
+      new MissingParameterError('field')
+    )
+    const error = systemUnderTest.validate({ field: 'any_value' })
+    expect(error).toEqual(new Error())
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const { systemUnderTest } = sutFactory()
+    const error = systemUnderTest.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
