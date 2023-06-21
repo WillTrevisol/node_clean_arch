38577bb25881a01b31ee3e6ddcbea8fd26abf4d1
Willian Trevisol <willianbylla@hotmail.com>
2023-06-20 21:40:07 -0300

    feat: add implementation and tests of HashCompare

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -4,6 +4,10 @@ import { BcryptAdapter } from './bcrypt-adapter'
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return Promise.resolve('hash')
+  },
+
+  async compare (): Promise<boolean> {
+    return Promise.resolve(true)
   }
 }))
 
@@ -13,23 +17,50 @@ const sutFactory = (): BcryptAdapter => {
 }
 
 describe('Bcrypt Adapter', () => {
-  test('Should call bcrypt with correct values', async () => {
+  test('Should call hash with correct values', async () => {
     const systemUnderTest = sutFactory()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await systemUnderTest.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  test('Should return a hash on success', async () => {
+  test('Should return a validHash on hash success', async () => {
     const systemUnderTest = sutFactory()
     const hash = await systemUnderTest.hash('any_value')
     expect(hash).toBe('hash')
   })
 
-  test('Should throw if bcrypt throws', async () => {
+  test('Should throw if hash throws', async () => {
     const systemUnderTest = sutFactory()
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
     const promise = systemUnderTest.hash('any_value')
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call compare with correct values', async () => {
+    const systemUnderTest = sutFactory()
+    const compareSpy = jest.spyOn(bcrypt, 'compare')
+    await systemUnderTest.compare('any_value', 'any_hash')
+    expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+  })
+
+  test('Should return true when compare succeeds', async () => {
+    const systemUnderTest = sutFactory()
+    const isValid = await systemUnderTest.compare('any_value', 'any_hash')
+    expect(isValid).toBe(true)
+  })
+
+  test('Should return false if compare fails', async () => {
+    const systemUnderTest = sutFactory()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => false)
+    const isValid = await systemUnderTest.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
+  })
+
+  test('Should throw if compare throws', async () => {
+    const systemUnderTest = sutFactory()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => { throw new Error() })
+    const promise = systemUnderTest.compare('any_value', 'any_hash')
     await expect(promise).rejects.toThrow()
   })
 })
--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt'
 import { type Hasher } from '../../data/protocols/criptography/hasher'
+import { type HashCompare } from '../../data/protocols/criptography/hash-compare'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashCompare {
   private readonly salt: number
 
   constructor (salt: number) {
@@ -11,5 +12,10 @@ export class BcryptAdapter implements Hasher {
   async hash (value: string): Promise<string> {
     const encrypedString = await bcrypt.hash(value, this.salt)
     return encrypedString
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
   }
 }
