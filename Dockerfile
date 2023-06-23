296a0a7a14f57fbe78dcec1dc9abcca86533c416
Willian Trevisol <willianbylla@hotmail.com>
2023-06-22 23:20:43 -0300

    chore: add docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM node:18
 WORKDIR /usr/library/clean-node-arch
 COPY ./package.json .
-RUN npm install
-COPY ./dist ./dist
-EXPOSE 5050
-CMD npm run start
+RUN npm install --only=prod
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://0.0.0.0:27017/node_clean_arch',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/node_clean_arch',
   port: process.env.port ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? '7h0L4=p3Dr0'
 }
