97186afd3277588c1d6e66b6fe46db5ec986e882
Willian Trevisol <willianbylla@hotmail.com>
2023-06-26 20:16:52 -0300

    feat: ensure AddSurveyController returns 500 if AddSurveyUseCase throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest } from '@/presentation/helpers/http/http-helper'
+import { badRequest, serverError } from '../../../helpers/http/http-helper'
 import { type HttpRequest, type HttpResponse, type Controller, type Validation, type AddSurvey } from './add-survey-protocols'
 
 export class AddSurveyController implements Controller {
@@ -8,19 +8,23 @@ export class AddSurveyController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
-    }
-    const { question, answers } = httpRequest.body
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { question, answers } = httpRequest.body
 
-    await this.addSurvey.add({
-      question,
-      answers
-    })
-    return Promise.resolve({
-      body: '',
-      statusCode: 200
-    })
+      await this.addSurvey.add({
+        question,
+        answers
+      })
+      return Promise.resolve({
+        body: '',
+        statusCode: 200
+      })
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
--- tests/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ tests/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { Validation, type HttpRequest, AddSurvey, AddSurveyModel } from '../../../../../src/presentation/controllers/survey/add-survey/add-survey-protocols'
 import { AddSurveyController } from '../../../../../src/presentation/controllers/survey/add-survey/add-survey-controller'
-import { badRequest } from '../../../../../src/presentation/helpers/http/http-helper'
+import { badRequest, serverError } from '../../../../../src/presentation/helpers/http/http-helper'
 
 const httpRequestFactory = (): HttpRequest => ({
   body: {
@@ -70,5 +70,14 @@ describe('AddSurvey Controller', () => {
     const httpResquest = httpRequestFactory()
     await systemUnderTest.handle(httpResquest)
     expect(addSpy).toHaveBeenCalledWith(httpResquest.body)
+  })
+
+  test('Should return 500 if AddSurvey throws', async () => {
+    const { systemUnderTest, addSurveyStub } = sutFactory()
+    jest.spyOn(addSurveyStub, 'add').mockReturnValueOnce(
+      Promise.reject(new Error)
+    )
+    const httpResponse = await systemUnderTest.handle(httpRequestFactory())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
