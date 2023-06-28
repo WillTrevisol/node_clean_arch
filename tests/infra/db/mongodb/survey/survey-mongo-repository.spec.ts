e5cf19a0abf55a4a766d6311e2737f6bcea22141
Willian Trevisol <willianbylla@hotmail.com>
2023-06-27 22:17:48 -0300

    feat: ensure SurveyMongoRepository add a new survey on success

---
+++ tests/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,41 @@
+import { MongoHelper } from '../../../../../src/infra/db/mongodb/helpers/mongo-helpers'
+import { SurveyMongoRepository } from '../../../../../src/infra/db/mongodb/survey/survey-mongo-repository'
+import { type Collection } from 'mongodb'
+
+describe('Survey Mongo Repository', () => {
+  let surveyCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getColletion('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  const sutFactory = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should add a survey on success', async () => {
+    const systemUnderTest = sutFactory()
+    await systemUnderTest.add({
+      question: 'any_question',
+      answers: [{
+        image: 'any_image',
+        answer: 'any_ answer'
+      },
+      {
+        answer: 'another_ answer'
+      }]
+    })
+
+    const survey = await surveyCollection.findOne({ question: 'any_question' })
+    expect(survey).toBeTruthy()
+  })
+})
