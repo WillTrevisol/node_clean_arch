148511d6fbfb4703610c8b9b0caed6efe3bda408
Willian Trevisol <willianbylla@hotmail.com>
2023-06-28 21:37:57 -0300

    feat: implemented decrypter on JwtAdapter

--- tests/infra/criptography/jwt-adapter/jwt-apapter.spec.ts
+++ tests/infra/criptography/jwt-adapter/jwt-apapter.spec.ts
@@ -1,16 +1,20 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from '../../../../src/infra/criptography/jwt-adapter/jwt-adapter'
 import { type Encrypter } from '../../../../src/data/protocols/criptography/encrypter'
+import { verify } from 'crypto'
 
 jest.mock('jsonwebtoken', () => ({
   sign: (): string => {
     return 'any_token'
+  },
+  verify: (token: string): string => {
+    return 'any_value'
   }
 })
 )
 
 interface SutTypes {
-  systemUnderTest: Encrypter
+  systemUnderTest: JwtAdapter
 }
 
 const sutFactory = (): SutTypes => {
@@ -42,6 +46,30 @@ describe('Jwt Adapter', () => {
         () => { throw new Error() }
       )
       const promise = systemUnderTest.encrypt('any_id')
+      await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('verify()', () => {
+    test('Should call verify with correct values', async () => {
+      const { systemUnderTest } = sutFactory()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await systemUnderTest.decrypt('any_token')
+      expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
+    })
+      
+    test('Should return a value on verify success', async () => {
+      const { systemUnderTest } = sutFactory()
+      const value = await systemUnderTest.decrypt('any_token')
+      expect(value).toBe('any_value')
+    })
+
+    test('Should throw if verify throws', async () => {
+      const { systemUnderTest } = sutFactory()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(
+        () => { throw new Error() }
+      )
+      const promise = systemUnderTest.decrypt('any_token')
       await expect(promise).rejects.toThrow()
     })
   })
