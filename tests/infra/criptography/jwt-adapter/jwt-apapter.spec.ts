04ad4237ab19d4ab9866b1383b235055839f7773
Willian Trevisol <willianbylla@hotmail.com>
2023-06-28 21:25:36 -0300

    refactor: separeted test on describe blocs

--- tests/infra/criptography/jwt-adapter/jwt-apapter.spec.ts
+++ tests/infra/criptography/jwt-adapter/jwt-apapter.spec.ts
@@ -22,25 +22,27 @@ const sutFactory = (): SutTypes => {
 }
 
 describe('Jwt Adapter', () => {
-  test('Should call sign with correct values', async () => {
-    const { systemUnderTest } = sutFactory()
-    const signSpy = jest.spyOn(jwt, 'sign')
-    await systemUnderTest.encrypt('any_id')
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
-  })
-
-  test('Should return a token o sing success', async () => {
-    const { systemUnderTest } = sutFactory()
-    const accessToken = await systemUnderTest.encrypt('any_id')
-    expect(accessToken).toBe('any_token')
-  })
-
-  test('Should throw if sign throws', async () => {
-    const { systemUnderTest } = sutFactory()
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(
-      () => { throw new Error() }
-    )
-    const promise = systemUnderTest.encrypt('any_id')
-    await expect(promise).rejects.toThrow()
+  describe('sign()', () => {
+    test('Should call sign with correct values', async () => {
+      const { systemUnderTest } = sutFactory()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await systemUnderTest.encrypt('any_id')
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+    })
+  
+    test('Should return a token o sing success', async () => {
+      const { systemUnderTest } = sutFactory()
+      const accessToken = await systemUnderTest.encrypt('any_id')
+      expect(accessToken).toBe('any_token')
+    })
+  
+    test('Should throw if sign throws', async () => {
+      const { systemUnderTest } = sutFactory()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(
+        () => { throw new Error() }
+      )
+      const promise = systemUnderTest.encrypt('any_id')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
