f49af07403e9d6f49e11fa47f393f4d485045392
Willian Trevisol <willianbylla@hotmail.com>
2023-07-05 08:39:33 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,18 @@
 import { type AddSurveyModel } from '../../../../domain/usecases/add-survey'
-import { type AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { type SurveyModel } from '../../../../domain/models'
+import { type AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
+import { type LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
 import { MongoHelper } from '../helpers/mongo-helpers'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getColletion('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getColletion('surveys')
+    const surveys = await surveyCollection.find().toArray()
+    return surveys
   }
 }
--- tests/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -1,12 +1,14 @@
 import { MongoHelper } from '../../../../../src/infra/db/mongodb/helpers/mongo-helpers'
 import { SurveyMongoRepository } from '../../../../../src/infra/db/mongodb/survey/survey-mongo-repository'
 import { type Collection } from 'mongodb'
+import MockDate from 'mockdate'
 
 describe('Survey Mongo Repository', () => {
   let surveyCollection: Collection
 
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
+    MockDate.set(new Date())
   })
 
   beforeEach(async () => {
@@ -16,27 +18,55 @@ describe('Survey Mongo Repository', () => {
 
   afterAll(async () => {
     await MongoHelper.disconnect()
+    MockDate.reset()
   })
 
   const sutFactory = (): SurveyMongoRepository => {
     return new SurveyMongoRepository()
   }
 
-  test('Should add a survey on success', async () => {
-    const systemUnderTest = sutFactory()
-    await systemUnderTest.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_ answer'
-      },
-      {
-        answer: 'another_ answer'
-      }],
-      date: new Date()
+  describe('add()', () => {
+    test('Should add a survey on success', async () => {
+      const systemUnderTest = sutFactory()
+      await systemUnderTest.add({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_ answer'
+        },
+        {
+          answer: 'another_ answer'
+        }],
+        date: new Date()
+      })
+  
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
+  })
 
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  describe('loadAll()', () => { 
+    test('Should load all surveys on success', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_ answer'
+        }],
+        date: new Date()
+      }, {
+        question: 'another_question',
+        answers: [{
+          image: 'another_image',
+          answer: 'another_ answer'
+        }],
+        date: new Date()
+      }])
+      const systemUnderTest = sutFactory()
+      const surveys = await systemUnderTest.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('another_question')
+    })
   })
 })
