df044028091dddd0c430c182a990439bba5ba0ee
Willian Trevisol <willianbylla@hotmail.com>
2023-07-11 13:02:07 -0300

    refactor: refactored mock survey models

--- tests/domain/mocks/mock-survey.ts
+++ tests/domain/mocks/mock-survey.ts
@@ -1,16 +1,6 @@
 import { type SurveyModel } from '@/domain/models'
 import { type AddSurveyParams } from '@/domain/usecases'
 
-export const mockSurveyModel = (): SurveyModel => ({
-  id: 'any_id',
-  question: 'any_question',
-  answers: [{
-    image: 'any_image',
-    answer: 'any_answer'
-  }],
-  date: new Date()
-})
-
 export const mockAddSurveyParams = (): AddSurveyParams => ({
   question: 'any_question',
   answers: [{
@@ -20,22 +10,11 @@ export const mockAddSurveyParams = (): AddSurveyParams => ({
   date: new Date()
 })
 
+export const mockSurveyModel = (): SurveyModel => Object.assign({}, mockAddSurveyParams(), ({ id: 'any_id' }))
+
 export const mockSurveyModelList = (): SurveyModel[] => {
-  return [{
-    id: 'any_id',
-    question: 'any_question',
-    answers: [{
-      image: 'any_image',
-      answer: 'any_answer'
-    }],
-    date: new Date()
-  }, {
-    id: 'another_id',
-    question: 'another_question',
-    answers: [{
-      image: 'another_image',
-      answer: 'another_answer'
-    }],
-    date: new Date()
-  }]
+  return [
+    mockSurveyModel(),
+    mockSurveyModel()
+  ]
 }
