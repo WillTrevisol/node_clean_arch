57cefe625cdd22f3bcd93b2f2f916ec7d3f30eea
Willian Trevisol <willianbylla@hotmail.com>
2023-07-12 11:40:53 -0300

    feat: add /login docs on swagger

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginPath } from './paths/login-path'
+import { loginParamsSchema } from './schemas'
+import { accountSchema } from './schemas/account-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Node Clean Arch',
     description: 'API feita no curso do Manguinho, para realizar enquetes entre programadores\n Aluno: Willian Trevisol',
     version: '1.6.1'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
