ecdd14bb6916dd77b6878220fcfb51d262da4fee
Willian Trevisol <willianbylla@hotmail.com>
2023-07-19 16:55:38 -0300

    refactor: move systemUnderTest creation to a factory method

--- tests/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ tests/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -3,16 +3,34 @@ import { DbLoadSurveyResult } from '@/data/usecases/survey-result/load-survey-re
 import { type SurveyResultModel } from '@/domain/models'
 import { mockSurveyResultModel } from '@/tests/domain/mocks'
 
+const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResultModel())
+    }
+  }
+  return new LoadSurveyResultRepositoryStub()
+}
+
+type SutTypes = {
+  systemUnderTest: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const sutFactory = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
+  const systemUnderTest = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+
+  return {
+    systemUnderTest,
+    loadSurveyResultRepositoryStub
+  }
+}
+
 describe('DbLoadSurvey UseCase', () => {
   test('Should call LoadSurveyResultRepository with correct value', async () => {
-    class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
-      async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
-        return Promise.resolve(mockSurveyResultModel())
-      }
-    }
-    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
+    const { systemUnderTest, loadSurveyResultRepositoryStub } = sutFactory()
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
-    const systemUnderTest = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
     await systemUnderTest.load('any_survey_id')
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')
   })
